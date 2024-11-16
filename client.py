24fcaf0fe1124b7e307ccf737d057cac0c060763
ofrikas <ofrikas1@gmail.com>
2024-11-16 16:54:06 +0200

    add comments with explanations

--- client.py
+++ client.py
@@ -1,7 +1,9 @@
 import socket
-
+# Create a UDP socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Send data
 s.sendto(b'Ofri Kastenbaum and Oran Zafrani', ('127.0.0.1', 12345))
+# Receive response
 data, addr = s.recvfrom(1024)
 print(str(data), addr)
 s.close()
--- server.py
+++ server.py
@@ -1,8 +1,11 @@
 import socket
-
+# Create a UDP socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Bind the socket to the port
 s.bind(('', 12345))
+# Listen for incoming data
 while True:
+    # Receive data
     data, addr = s.recvfrom(1024)
     print(str(data), addr)
     s.sendto(data.upper(), addr)
